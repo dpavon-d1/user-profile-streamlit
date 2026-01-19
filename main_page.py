52a20aafef862089077a234d7a7ce56bb5b8677d
dpavon-d1 <mpavon@infobae.com>
2026-01-19 11:20:46 -0300

    fix - main page

--- main_page.py
+++ main_page.py
@@ -58,7 +58,7 @@ st.markdown("""
     background-color: #ffffff;
     border-radius: 8px;
     padding: 10px 8px;
-    box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+    box-shadow: 0 4px 4px rgba(0,0,0,0.1);
     border-left: 2px solid #F28322;
     display: flex;
     flex-direction: column;
