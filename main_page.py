902d80771a36c1e6599c5a02c2cdbd4b2d2cd07d
dpavon-d1 <mpavon@infobae.com>
2026-01-19 10:40:00 -0300

    fix - main page

--- main_page.py
+++ main_page.py
@@ -195,8 +195,13 @@ with col_grafico:
     fig_device.update_layout(
         barmode='group',
         title='Según Dispositivo y Estado',
-        # yaxis={'title': {'text': 'Usuarios'}},
-        # xaxis={'title': {'text': 'Estado Usuario'}},
+        xaxis={
+            'categoryorder': 'array',
+            'categoryarray': estados,
+            'tickmode': 'array',
+            'tickvals': estados,
+            'ticktext': estados
+        },
         legend={
             'orientation': 'h',
             'yanchor': 'bottom',
@@ -204,7 +209,9 @@ with col_grafico:
             'xanchor': 'right',
             'x': 1
         },
-        height=400
+        height=400,
+        bargap=0.2,
+        bargroupgap=0.1
     )
     st.plotly_chart(fig_device, use_container_width=True)
 
@@ -235,8 +242,15 @@ with col_vacia:
     fig_sesion_historial.update_layout(
         barmode='group',
         title='Según historial de sesiones',
-        bargap=0.5,
-        bargroupgap=0.02,
+        xaxis={
+            'categoryorder': 'array',
+            'categoryarray': estados,
+            'tickmode': 'array',
+            'tickvals': estados,
+            'ticktext': estados
+        },
+        bargap=0.2,
+        bargroupgap=0.1,
         legend={
             'orientation': 'h',
             'yanchor': 'bottom',
@@ -244,7 +258,7 @@ with col_vacia:
             'xanchor': 'right',
             'x': 1
         },
-        height=400  # Misma altura que los otros gráficos
+        height=400
     )
 
 
