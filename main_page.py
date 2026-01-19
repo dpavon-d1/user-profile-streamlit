bc6c68b2e1f9c6fa0fa5c0c7593fdad7bb0a4cc2
dpavon-d1 <mpavon@infobae.com>
2026-01-19 12:31:47 -0300

    fix - main page

--- main_page.py
+++ main_page.py
@@ -100,7 +100,7 @@ div[data-testid="stAlert"] > div {
 }
 div[data-testid="stAlert"] p {
     color: #000000 !important;
-    font-size: 20px !important;
+    font-size: 16px !important;
 }
 </style>
 """, unsafe_allow_html=True)
