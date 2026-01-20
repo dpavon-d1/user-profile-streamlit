bda2db17012b694f859da5eac8f285a576c75a2b
dpavon-d1 <mpavon@infobae.com>
2026-01-20 13:30:11 -0300

    fix - main page

--- charts/funnel/css.py
+++ charts/funnel/css.py
@@ -9,9 +9,9 @@ FUNNEL_OPACITY = DEFAULT_OPACITY
 
 # === FUNNEL COLORS (with opacity) ===
 COLORS = {
-    "primary": hex_to_rgba("#439FD9", FUNNEL_OPACITY),
-    "primary_dark": hex_to_rgba("#94D7F2", FUNNEL_OPACITY),
-    "secondary": hex_to_rgba("##F28322", FUNNEL_OPACITY),
+    "primary": hex_to_rgba("#2450A6", FUNNEL_OPACITY),
+    "primary_dark": hex_to_rgba("#439FD9", FUNNEL_OPACITY),
+    "secondary": hex_to_rgba("#94D7F2", FUNNEL_OPACITY),
     "connector": hex_to_rgba("##F2F2DC", 0.4)
 }
 
